d7ef1e764deeaf45102ceb213ba2eb1438620049
mukeshprajapat-star <mukeshprajapat7529@gmail.com>
2024-04-13 20:22:16 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
   const {user,loader} =useSelector((state)=>state.auth)
   const dispatch=useDispatch()
   useEffect(() => {
-    axios.get(`${server}/api/v1/user/me`,{withCredentials:true})
+    axios.get(`/api/v1/user/me`,{withCredentials:true})
     .then(({data})=>dispatch(userExists(data.user)))
     .catch((err)=>dispatch(userNotExists()))
   }, [dispatch])
