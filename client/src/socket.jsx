b490541cbe51be78366372426ec2421ebfdd122e
mukeshprajapat-star <mukeshprajapat7529@gmail.com>
2024-04-13 21:00:30 +0530

    done

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -5,7 +5,7 @@ const SocketContext=createContext();
 const getSocket=() =>useContext(SocketContext)
 const SocketProvider=({children})=>{
     const socket =useMemo(
-        ()=> io(server,{withCredentials:true}) ,[]);
+        ()=> io("http://localhost:3000",{withCredentials:true}) ,[]);
 
     return (
         <SocketContext.Provider value={socket}>
